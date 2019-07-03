d7ed3aaf2463186bd93623df26a80cce3ef532db
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-03 17:38:26 +0300

    added task 13

--- WhiteBelt/Task_013.cpp
+++ WhiteBelt/Task_013.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+void UpdateIfGreater(const int& first, int& second) {
+	if (first > second) second = first;
+}
+
+#define DPRINT(ARG) std::cout << #ARG << ": " << ARG << std::endl
+
 int main() {
+	int a = 4;
+	int b = 2;
+
+	UpdateIfGreater(a, b);
+
+	DPRINT(a);
+	DPRINT(b);
 
 	return 0;
 }
