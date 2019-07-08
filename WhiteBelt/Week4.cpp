c40b0f47fe826686c1800ecafdd4e05c5070e020
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-08 21:47:24 +0300

    update week 4

--- WhiteBelt/Week4.cpp
+++ WhiteBelt/Week4.cpp
@@ -47,8 +47,31 @@ void example_writefile(std::string filename = "hello_out.txt") {
 	example_readfile(filename);
 }
 
+void example_string_view(std::string_view sv) {
+	std::cout << sv << std::endl;
+}
+
+void test_example_string_view() {
+	example_string_view("test 0");
+
+	const char* test1 = "test 1";
+	example_string_view(test1);
+
+	std::string test2 { "test 2" };
+	example_string_view(test2);
+
+	const std::string test3 { "test 3" };
+	example_string_view(test3);
+
+	std::string test4 { "test 4" };
+	example_string_view(test4.c_str());
+
+	const std::string test5 { "test 5" };
+	example_string_view(test5.c_str());
+}
+
 int main(int argc, char** argv) {
-	example_writefile();
+	test_example_string_view();
 
 	return 0;
 }
