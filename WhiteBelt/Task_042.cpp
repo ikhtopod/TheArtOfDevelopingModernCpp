d74fd5c7e0d303f70d8aad79c7555b505d16f93a
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-08 15:42:18 +0300

    added task 42

---
+++ WhiteBelt/Task_042.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Name {
+	std::string first {};
+	std::string last {};
+};
+
+struct Date {
+	int day = 0;
+	int month = 0;
+	int year = 0;
+};
+
+struct Student {
+	Name name {};
+	Date date {};
+};
+
+int main() {
+	int n = 0;
+	std::cin >> n;
+
+	std::vector<Student> students(n);
+	for (auto& s : students) {
+		std::cin >> s.name.first >> s.name.last;
+		std::cin >> s.date.day >> s.date.month >> s.date.year;
+	}//rof
+
+	int m = 0;
+	std::cin >> m;
+
+	for (int i = 0; i < m; ++i) {
+		std::string cmd {};
+		int num = 0;
+
+		std::cin >> cmd >> num;
+
+		if (num > 0 && num <= 10'000) {
+			num--;
+
+			if (cmd == "name") {
+				std::cout << students[num].name.first << " " <<
+					students[num].name.last << std::endl;
+			} else if (cmd == "date") {
+				std::cout << students[num].date.day << "." <<
+					students[num].date.month << "." <<
+					students[num].date.year << std::endl;
+			} else {
+				std::cout << "bad request" << std::endl;
+			}//fi
+		} else {
+			std::cout << "bad request" << std::endl;
+		}//fi
+	}//rof
+
+	return 0;
+}
