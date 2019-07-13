5f25a0400b37a33f649720e2a789d08feeca41dc
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-13 21:54:26 +0300

    update week2.cpp

--- YellowBelt/Week2.cpp
+++ YellowBelt/Week2.cpp
@@ -9,12 +9,20 @@ void Sum_Test() {
 	assert(Sum(2, 3) == 5);
 	assert(Sum(-2, -3) == -5);
 	assert(Sum(-2, 0) == -2);
+	//assert(Sum(-2, 0) == 2); // wrong
+	assert(Sum(-2, 2) == 0);
+}
+void Sum_Test2() {
+	assert(Sum(2, 3) == 5);
+	assert(Sum(-2, -3) == -5);
+	assert(Sum(-2, 0) == -2);
 	assert(Sum(-2, 0) == 2); // wrong
 	assert(Sum(-2, 2) == 0);
 }
 
 void RunAllTests() {
 	RUN_TEST(Sum_Test());
+	RUN_TEST(Sum_Test2());
 }
 
 int main() {
