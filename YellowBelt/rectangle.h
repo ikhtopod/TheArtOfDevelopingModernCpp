07ac897237820183d9e4fdd51aa642c668d18aa3
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-19 19:49:05 +0300

    added task 15

---
+++ YellowBelt/rectangle.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// task 15
+
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle {
+public:
+	Rectangle(int width, int height);
+
+	int GetArea() const;
+
+	int GetPerimeter() const;
+
+	int GetWidth() const;
+	int GetHeight() const;
+
+private:
+	int width_, height_;
+};
+
+#endif // !RECTANGLE_H
