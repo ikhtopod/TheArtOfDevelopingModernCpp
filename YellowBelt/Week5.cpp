bfc2b50cb011f9f2183f8155179c2d43b63dbae1
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-23 22:23:32 +0300

    update week5.cpp

--- YellowBelt/Week5.cpp
+++ YellowBelt/Week5.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int main() {
+class Base {
+public:
+	virtual void Print() const {
+		std::cout << "Base" << std::endl;
+	}
+};
 
+class Derived : public Base {
+public:
+	void Print() const override {
+		std::cout << "Derived" << std::endl;
+	}
+};
+
+int main() {
+	Derived d {};
+	d.Print();
+
+	Base b {};
+	b.Print();
+
+	Base* bd = new Derived {};
+	bd->Print();
+	delete bd;
 
 	return 0;
 }
