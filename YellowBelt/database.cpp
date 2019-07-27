f228ad5446e1ef90fa1485c3298914450709b832
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-07-27 22:52:28 +0300

    updated database.h/cpp

--- YellowBelt/database.cpp
+++ YellowBelt/database.cpp
@@ -6,6 +6,18 @@ Date ParseDate(std::istream& is) {
 	return date;
 }
 
-void Database::Add(Date date, std::string event) {
+void Database::Add(const Date& date, const std::string& event) {
 	Add(date, Event { event });
+}
+
+void Database::Add(const Date& date, const Event& event) {
+	m_db[date].Add(event);
+}
+
+void Database::Print(std::ostream& os) {
+	for (const auto& [date, events] : m_db) {
+		for (const auto& event : events.GetValue()) {
+			std::cout << date << " " << event << std::endl;
+		}//rof
+	}//rof
 }
--- YellowBelt/database.h
+++ YellowBelt/database.h
@@ -12,17 +12,17 @@ private:
 	std::map<Date, Events> m_db {};
 
 public:
-	void Add(Date date, std::string event);
-	void Add(Date date, Event event);
+	void Add(const Date& date, const std::string& event);
+	void Add(const Date& date, const Event& event);
 
-	std::string Last(Date date);
+	std::string Last(const Date& date);
 	void Print(std::ostream& os);
 
 	template<typename Pred>
-	int RemoveIf(Pred pred);
+	int RemoveIf(Pred& pred);
 
 	template<typename Pred>
-	std::string FindIf(Pred pred);
+	std::string FindIf(Pred& pred);
 };
 
 Date ParseDate(std::istream& is);
