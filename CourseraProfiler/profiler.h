12f07ada463fb09340a565ea527d6d53d69bcd43
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-08-02 16:13:07 +0300

    added macros for test with uniq_id

--- CourseraProfiler/profiler.h
+++ CourseraProfiler/profiler.h
@@ -3,6 +3,15 @@
 #ifndef COURSERAPROFILER_PROFILE_H
 #define COURSERAPROFILER_PROFILE_H
 
+
+#define PROFILER_CONCAT_ID(counter) __feawykgu_LogDuration_uniq_id ## counter
+#define PROFILER_MAKE_ID(counter) PROFILER_CONCAT_ID(counter)
+#define PROFILER_UNIQ_ID PROFILER_MAKE_ID(__COUNTER__)
+
+#define LOG_DURATION(message) \
+	LogDuration PROFILER_UNIQ_ID { message }
+
+
 #include <iostream>
 #include <string>
 #include <ratio>
