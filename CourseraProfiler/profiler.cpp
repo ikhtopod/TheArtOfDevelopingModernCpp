1bc2d09ad767ef2db87390b803141fa2aa95a6d8
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-08-02 15:51:11 +0300

    added first varian profiler

--- CourseraProfiler/profiler.cpp
+++ CourseraProfiler/profiler.cpp
@@ -1,1 +1,14 @@
 #include "profiler.h"
+
+using namespace std::chrono;
+
+LogDuration::LogDuration() :
+	m_start(steady_clock::now()) {}
+
+LogDuration::~LogDuration() {
+	time_point<steady_clock> finish = steady_clock::now();
+	nanoseconds dur_nano = finish - m_start;
+	milliseconds dur_milli = duration_cast<milliseconds>(dur_nano);
+	DoubleSecond dur_second_double = duration_cast<DoubleSecond>(dur_nano);
+	std::cerr << dur_milli.count() << " ms (" << dur_second_double.count() << " s)" << std::endl;
+}
--- CourseraProfiler/profiler.h
+++ CourseraProfiler/profiler.h
@@ -1,2 +1,22 @@
 #pragma once
 
+#ifndef COURSERAPROFILER_PROFILE_H
+#define COURSERAPROFILER_PROFILE_H
+
+#include <iostream>
+#include <ratio>
+#include <chrono>
+
+class LogDuration final {
+	using DoubleSecond = std::chrono::duration<double, std::ratio<1, 1>>;
+
+private:
+	std::chrono::steady_clock::time_point m_start;
+
+public:
+	LogDuration();
+	~LogDuration();
+};
+
+
+#endif // !COURSERAPROFILER_PROFILE_H
